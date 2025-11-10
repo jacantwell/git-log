3c015f5ef49b53383ee71b6becbc9ca7ca87bf8e
Jasper Cantwell <jasper66018@gmail.com>
2025-11-10 14:00:52 +0000

    Fixing pr querey

--- internal/github/pullrequests.go
+++ internal/github/pullrequests.go
@@ -6,17 +6,15 @@ import (
     "fmt"
     "time"
 )
-
 func (c *Client) GetPullRequests(author string, since time.Time) ([]IssueSearchResultItem, error) {
-    query := fmt.Sprintf("type:pr author:%s author-date:>%s", author, since.Format(time.RFC3339))
-    // query := fmt.Sprintf("type:pr author-date:>%s", since.Format(time.RFC3339))
+    query := fmt.Sprintf("is:pr author:%s created:>%s", author, since.Format(time.RFC3339))
 
     // Build URL with properly encoded query parameters
     baseURL := fmt.Sprintf("%s/search/issues", c.BaseURL)
     params := url.Values{}
     params.Add("q", query)
     params.Add("per_page", "100")
-    params.Add("sort", "author-date")
+    params.Add("sort", "created")
     params.Add("order", "desc")
 
     requestURL := fmt.Sprintf("%s?%s", baseURL, params.Encode())
